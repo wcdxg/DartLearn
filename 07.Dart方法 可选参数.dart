a29ca0b88b3b2a5740dc97aa93fe95d0337b3108
Yuaihen <706375160@qq.com>
2019-09-19 17:31:19 +0800

    Dart方法可选参数  方法当做参数

---
+++ 07.Dart方法 可选参数.dart
@@ -0,0 +1,34 @@
+void main() {
+//1.定义一个带可选参数的方法
+  String printUserInfo(String userName, [int age, String sex = '男']) {
+    if (age != null) {
+      return "姓名$userName 年龄$age 性别$sex";
+    } else {
+      return "姓名$userName 年龄保密 性别$sex";
+    }
+  }
+
+  print(printUserInfo('张三', 20, '女'));
+
+  //2.匿名参数
+  String printUserInfo2(String userName, {int age, String sex = '男'}) {
+    if (age != null) {
+      return "姓名$userName 年龄$age 性别$sex";
+    } else {
+      return "姓名$userName 年龄保密 性别$sex";
+    }
+  }
+
+  print(printUserInfo2('李四', age: 40));
+
+  //3.方法当做参数
+  void fn1() {
+    print('我是fn1方法');
+  }
+
+  void fn2(aa) {
+    aa();
+  }
+
+  fn2(fn1);
+}
